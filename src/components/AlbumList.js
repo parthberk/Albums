46963a76515d72b66153910bb8a80924469e74bb
Parth <parthx@live.com>
2019-06-12 18:31:18 -0700

    List of Albums

---
+++ src/components/AlbumList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+const AlbumList=()=>{
+    return (
+        <View>Album List</View>
+    );
+};
+
+
+export default AlbumList;
