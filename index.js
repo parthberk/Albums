ad796bfff1802af78ade3e4310922ce111e65048
Parth <parthx@live.com>
2019-06-13 03:34:01 -0700

    AlbumList Updated

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // Import library to help create a content
 
 import React from 'react';
-import {AppRegistry} from 'react-native';
-import Header from './src/components/header';
+import {AppRegistry,View} from 'react-native';
+import Header from './src/components/Header';
+import Albumlist from './src/components/AlbumList';
 
 
 //Create a component
 //To create a componenet we'll write a javascript function
 const App=()=>
-(
+(   <View>
     <Header headerText={'Albums'}/>
+    <Albumlist/>
+    </View>
 );
 //Render it to the device
 AppRegistry.registerComponent('albums',()=> App);
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Text,View} from 'react-native';
 
+
 const AlbumList=()=>{
     return (
-        <View>Album List</View>
+        <View>
+        <Text>Album List</Text>
+        </View>
     );
 };
 
